27da8ae5f35c3b061ff02914c3a8daa121c1ddae
Mutyala Harshith <105526704+Mega-Hero@users.noreply.github.com>
2022-07-06 08:51:14 +0530

    Update mhinfo.py

--- mhinfo.py
+++ mhinfo.py
@@ -21,35 +21,30 @@ BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton(text="💞 Join", url=f"ht
                                  InlineKeyboardButton(text="Support", url=f"https://t.me/MHGcHaT")]])
 
 
-@Bot.on_message(filters.private & filters.command("start"))
-async def start(bot, update):
+
+
+@Bot.on_message(filters.command(["start"]))
+async def start(_, message):
     await message.reply_photo(
         photo="https://telegra.ph/file/236794ce4bb2213eaae1e.jpg",
-        caption=START_TEXT.format(update.from_user.mention),
-        reply_markup=BUTTONS
+        caption=START_TEXT.format(message.from_user.mention),
+        reply_markup=START_BUTTONS,
+        quote=True
     )
 
-@Bot.on_message(filters.private & filters.command("myinfo"))
-async def start(bot, update):
-    await update.reply_text(
-        text=MHSTART.format(update.from_user.mention),
-        disable_web_page_preview=True,
-        reply_markup=BUTTONS
-    )
 
 @Bot.on_message(filters.private & filters.command("mhinfo"))
 async def info(bot, update):
     
     text = f"""--**Information from Harshith**--
-**💞 First Name :** {update.from_user.first_name}
-**😎 Your Second Name :** {update.from_user.last_name if update.from_user.last_name else 'None'}
-**🥳 Your Username :** {update.from_user.username}
-**😜 Your Telegram ID :** {update.from_user.id}
-**🤫 Your Profile Link :** {update.from_user.mention}
-
-**DC ID:** {update.from_user.dc_id}
-**Language Code:** {update.from_user.language_code if update.from_user.language_code else 'None'}
-**Status:** {update.from_user.status if update.from_user.status else 'None'}
+**💞 Your Name :** {update.from_user.first_name}
+**😎 Second Name :** {update.from_user.last_name if update.from_user.last_name else 'None'}
+**🥳 YouR Username :** {update.from_user.username}
+**😜 Your TG ID :** {update.from_user.id}
+**🤫 Profile Link :** {update.from_user.mention}
+**✨ DC ID:** {update.from_user.dc_id}
+**💖 Language Code:** {update.from_user.language_code if update.from_user.language_code else 'None'}
+**🤩 Status:** {update.from_user.status if update.from_user.status else 'None'}
 """
     
     await update.reply_text(        
